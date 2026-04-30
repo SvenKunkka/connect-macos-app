c1728d644f51ece64d3528a3b45069c4cf6fa603
Yujian <cheirsh6@gmail.com>
2026-04-30 08:55:01 +0800

    Initial Commit

---
+++ Connect/ConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectApp.swift
+//  Connect
+//
+//  Created by Yujian on 2026/4/30.
+//
+
+import SwiftUI
+
+@main
+struct ConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
